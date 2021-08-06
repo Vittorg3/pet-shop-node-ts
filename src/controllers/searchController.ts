7dde1b26caaa59b7db1de7e243945f321f12543a
Enoc Vitor <vittor_g3@hotmail.com>
2021-08-05 21:10:45 -0300

    final project 1.0

--- src/controllers/searchController.ts
+++ src/controllers/searchController.ts
@@ -1,5 +1,16 @@
 import { Request, Response } from 'express';
 
-export const search = (req: Request, res: Response) => {
+import { Pet } from '../models/pet';
+import { createMenuObject } from '../helpers/createMenuObject';
 
+export const search = (req: Request, res: Response) => {
+    let query: string = req.query.q as string;
+
+    !query && res.redirect('/');
+
+    res.render('pages/page', {
+        menu: createMenuObject(''),
+        list: Pet.getFromName(query),
+        query
+    });
 };
